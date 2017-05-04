469ed51dfb03a98a2a7222a70f18bb3f99217ca4
Ian Tracy <iatr@hms.se>
2017-05-04 07:53:06 -0400

    add exercise 2.21

--- chapter1/ex1_21/main.cpp
+++ chapter1/ex1_21/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include "sales_item.h"
 
-/* write a program that reads a set of book sales transactions,
-    writing each transactions to the std output */
+/* write a program that reads two sales_items objects and produces thier sum( if they have the same isbn) */
 
 int main( int argc, char* argv[]){
-  std::cout << "Enter sales transactions"  << std::endl;
-  Sales_item item1;
-  while( std::cin >> item1 ){
-    std::cout << item1 << std::endl;
+  std::cout << "Enter sales transactions, you should see the sum for "\
+    "two consecutive isbns"  << std::endl;
+  Sales_item old, nnew ;
+  std::cin >> old;
+  while( std::cin >> nnew ){
+    if( nnew.isbn() == old.isbn() ){
+      std::cout << nnew + old << std::endl;
+    }else{
+      old = nnew;
+    }
   }
+  return 0;
 }
 
 
